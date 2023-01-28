25c8f0226aa37a9c12df1d10b891d69eb52709c2
dung <doantrungdung2001@gmail.com>
2023-01-28 22:52:49 +0700

    create database

---
+++ server/createDB.c
@@ -0,0 +1,96 @@
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+  MYSQL *con = mysql_init(NULL);
+
+  if (con == NULL)
+  {
+    printf("%s\n", mysql_error(con));
+    exit(1);
+  }
+
+  char *server = "127.0.0.1";
+  char *user = "root";
+  char *password = "Dung19102001";
+
+  if (mysql_real_connect(con, server, user, password, NULL, 0, NULL, 0) == NULL)
+  {
+    printf("%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+  }
+  if (!mysql_set_character_set(con, "utf8"))
+  {
+    printf("New client character set: %s\n",
+           mysql_character_set_name(con));
+  }
+
+  //****CREATE DATABASE*****
+  if (mysql_query(con, "CREATE DATABASE IF NOT EXISTS Share_Image CHARACTER SET utf8 COLLATE utf8_unicode_ci"))
+  {
+    if (strcmp(mysql_error(con),
+               "Can't create database 'test'; database exists") == 0)
+    {
+      printf("Database is exists");
+    }
+    else
+    {
+      fprintf(stderr, "%s\n", mysql_error(con));
+      mysql_close(con);
+      exit(1);
+    }
+  }
+  printf("%s\n", "Create database succesfully ...");
+
+  // ****SELECT DATABASE****
+  if (mysql_query(con, "USE Share_Image"))
+  {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+  }
+  printf("%s\n", "Using database ...");
+
+  // ****CREATE USER TABLE****
+  if (mysql_query(con, "DROP TABLE IF EXISTS users"))
+  {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+  }
+
+  if (mysql_query(
+          con,
+          "CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255) UNIQUE, password VARCHAR(255))"))
+  {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+  }
+  printf("%s\n", "Create table users succesfully ...");
+
+  // if (mysql_query(
+  //         con,
+  //         "CREATE TABLE using_accounts(id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255) UNIQUE)"))
+  // {
+  //   fprintf(stderr, "%s\n", mysql_error(con));
+  //   mysql_close(con);
+  //   exit(1);
+  // }
+
+  if (mysql_query(
+          con,
+          "INSERT INTO users(username, password) VALUES('admin', '123456'), ('dungdoan', '191001')"))
+  {
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+  }
+
+  // mysql_close(con);
+  // exit(0);
+}
